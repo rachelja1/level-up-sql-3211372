42d28d1d7ba143e29a83a00a12f47fcb250b0660
rachelja1 <115790228+rachelja1@users.noreply.github.com>
2023-03-20 16:53:54 +0000

    printmenus

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,15 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+SELECT * from Dishes
+ORDER BY Price;
+
+SELECT * from Dishes
+WHERE Type = "Beverage" or Type = "Appetizer"
+ORDER BY Type;
+
+SELECT * from Dishes
+WHERE Type != "Beverage"
+ORDER BY Type;
+
