7d27054f52ea62203910d735919e88f25f9bb1b0
rachelja1 <115790228+rachelja1@users.noreply.github.com>
2023-03-21 12:57:31 +0000

    visit reminder

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,10 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT p.FirstName,p.LastName, COUNT(DISTINCT LoanID) as numLoans
+FROM Loans as l
+JOIN Patrons as p
+ON l.PatronID = p.PatronID
+GROUP BY p.PatronID
+ORDER BY numLoans
+LIMIT 15;
