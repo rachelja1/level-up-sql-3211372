b25755b57fcaebb8d6ce9587be09020ffb91e334
rachelja1 <115790228+rachelja1@users.noreply.github.com>
2023-03-21 11:07:07 +0000

    attend event + reservations

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,8 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT * FROM Reservations as r join Customers as c
+ON r.CustomerID = c.CustomerID
+WHERE (c.LastName LIKE "Ste%ens%n" or c.LastName = "Stuyvesant") and r.PartySize = 4
+ORDER BY r.Date desc;
--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,25 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+-- find customer
+SELECT * from Customers
+WHERE FirstName = "Sam" and LastName = "McAdams";
+-- does not exist
+
+INSERT INTO Customers
+  (FirstName, LastName, Email)
+VALUES (
+  "Sam", "McAdams", "smac@kinetecoinc.com"
+);
+
+INSERT INTO Reservations
+  (CustomerID, Date, PartySize)
+VALUES
+  (102, "2022-08-12 18:00:00", 5)
+;
+
+SELECT c.FirstName, c.LastName, c.Email, r.Date, r.PartySize
+FROM Customers c join Reservations r
+ON c.CustomerID = r.CustomerID
+WHERE c.Email = "smac@kinetecoinc.com";
