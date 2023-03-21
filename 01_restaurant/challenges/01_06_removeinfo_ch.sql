e2881be2f55f33d7b82b08273a76daada262d61f
rachelja1 <115790228+rachelja1@users.noreply.github.com>
2023-03-21 10:38:56 +0000

    remove info

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,12 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT * from Customers as c join Reservations as r 
+ON r.CustomerID = c.CustomerID
+WHERE c.FirstName = "Norby" and r.Date > "2022-07-24";
+
+DELETE FROM Reservaitons WHERE ReservationID = 2000;
+
+-- or 
+UPDATE Reservations SET Date = NULL WHERE ReservationID = 2000;
