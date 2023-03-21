9113e5ab0cc66f401b38a9f5323d6602f115fa68
rachelja1 <115790228+rachelja1@users.noreply.github.com>
2023-03-21 12:53:35 +0000

    return books

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,14 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+-- update Loans with Returned date
+
+UPDATE Loans
+SET ReturnedDate = "2022-06-05"
+WHERE BookID in (
+  SELECT BookID FROM Books 
+  WHERE Barcode in (6435968624, 5677520613, 8730298424)
+) AND ReturnedDate is NULL;
+
+SELECT * from Loans where ReturnedDate = "2022-06-05";
