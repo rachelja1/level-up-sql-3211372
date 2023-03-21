b27e3e9174db687b8278d35901fe240c73eaa727
rachelja1 <115790228+rachelja1@users.noreply.github.com>
2023-03-21 12:28:28 +0000

    add book + checkout

--- 02_library/challenges/02_02_addbook_ch.sql
+++ 02_library/challenges/02_02_addbook_ch.sql
@@ -9,3 +9,12 @@
 -- Author: Jonathan Swift
 -- Published: 1729
 -- ID Number: 4899254401
+
+INSERT INTO Books (Title, Author, Published, Barcode)
+VALUES
+  ("Dracula", "Bram Stoker", 1897, 4819277482),
+  ("Gulliver’s Travels into Several Remote Nations of the World", "Jonathan Swift", 1729, 4899254401)
+;
+
+SELECT * from Books
+ORDER BY BookID desc;
--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,26 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+-- find BookID
+SELECT BookID from Books WHERE Barcode = "2855934983";
+SELECT BookID from Books WHERE Barcode = "4043822646";
+
+-- find PatronID
+SELECT PatronID from Patrons WHERE Email = "jvaan@wisdompets.com";
+
+-- solution - one query
+INSERT INTO Loans (BookID, PatronID, LoanDate, DueDate)
+VALUES
+  ( (SELECT BookID from Books WHERE Barcode = "2855934983"),
+    (SELECT PatronID from Patrons WHERE Email = "jvaan@wisdompets.com"),
+    "2022-08-25", "2022-09-08"
+  ),
+  ( (SELECT BookID from Books WHERE Barcode = "4043822646"),
+    (SELECT PatronID from Patrons WHERE Email = "jvaan@wisdompets.com"),
+    "2022-08-25", "2022-09-08"
+  );
+
+
+SELECT * FROM Loans 
+ORDER BY DueDate desc;
